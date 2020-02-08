a000b6d4a684bea9a384471c00011ad91ecf4743
H37kouya <h37kouya@gmail.com>
2020-02-08 11:05:38 +0900

    [add]: apiのルーティングにバージョン追加

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', 'Auth\LoginController@logout');
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('logout', 'Auth\LoginController@logout');
+
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+        Route::patch('settings/profile', 'Settings\ProfileController@update');
+        Route::patch('settings/password', 'Settings\PasswordController@update');
     });
 
-    Route::patch('settings/profile', 'Settings\ProfileController@update');
-    Route::patch('settings/password', 'Settings\PasswordController@update');
-});
+    Route::group(['middleware' => 'guest:api'], function () {
+        Route::post('login', 'Auth\LoginController@login');
+        Route::post('register', 'Auth\RegisterController@register');
 
-Route::group(['middleware' => 'guest:api'], function () {
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('register', 'Auth\RegisterController@register');
+        Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+        Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+        Route::post('email/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
+        Route::post('email/resend', 'Auth\VerificationController@resend');
 
-    Route::post('email/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
-    Route::post('email/resend', 'Auth\VerificationController@resend');
-
-    Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
-    Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+        Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
+        Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+    });
 });
