2b362cb255040d9ee0df49f935e74eddb947bcbb
H37kouya <h37kouya@gmail.com>
2020-02-09 22:38:24 +0900

    [add]: storageのmodelを追加

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * App\Models\Storage
@@ -20,5 +21,36 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Storage extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * 複数代入する属性
+     *
+     * @var array
+     */
+    protected $fillable = ['storage_id', 'user_id', 'title', 'description', 'long_comment', 'storage_url', 'eyecatch_imgae_url', 'web_address'];
+
+    /**
+     * ネイティブなタイプへキャストする属性
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id'                 => 'integer',
+        'storage_id'         => 'string',
+        'user_id'            => 'integer',
+        'title'              => 'string',
+        'description'        => 'string',
+        'long_comment'       => 'string',
+        'storage_url'        => 'string',
+        'eyecatch_imgae_url' => 'string',
+        'web_address'        => 'string'
+    ];
+
+    /**
+     * 日付へキャストする属性
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
--- database/migrations/2020_02_09_082420_create_storages_table.php
+++ database/migrations/2020_02_09_082420_create_storages_table.php
@@ -15,6 +15,15 @@ class CreateStoragesTable extends Migration
     {
         Schema::create('storages', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('storage_id', '50')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title', '50')->comment('作品名');
+            $table->string('description', '50')->nullable()->comment('一言コメント');
+            $table->longText('long_comment')->nullable()->comment('長文コメント');
+            $table->string('storage_url', '255')->nullable()->comment('ストレージURL');
+            $table->string('eyecatch_imgae_url', '255')->comment('アイキャッチ画像URL');
+            $table->string('web_address', '255')->comment('WEB Address');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
