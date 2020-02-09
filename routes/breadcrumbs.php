f5485475e19dc85e79584350a0ca79e728ab631b
H37kouya <h37kouya@gmail.com>
2020-02-09 17:16:41 +0900

    [add]: client_urlを作成

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Facades\Helper as H;
+
 // Home
 Breadcrumbs::for('home', function ($trail) {
-    $trail->push('Home', '/');
+    $trail->push('Home', H::client_route('/'));
 });
 
 // Home > blog
 Breadcrumbs::for('blog', function ($trail) {
     $trail->parent('home');
-    $trail->push('Blog', '/blog');
+    $trail->push('Blog', H::client_route('/blog'));
 });
