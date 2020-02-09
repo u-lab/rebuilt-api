7dc6e3cec964badd4a930146e54aa1b914f31298
H37kouya <h37kouya@gmail.com>
2020-02-09 16:47:18 +0900

    [add]: api routingにnameを追加

--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,24 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1'], function () {
     // パンくずリスト
-    Route::get('breadcrumbs', 'BreadcrumbsController@index');
+    Route::get('breadcrumbs', 'BreadcrumbsController@index')->name('breadcrumbs.index');
     // サイトマップ
-    Route::get('sitemap', 'SitemapController@index');
+    Route::get('sitemap', 'SitemapController@index')->name('sitemap.index');
 
     // pages
     Route::group(['prefix' => 'pages'], function () {
         // 個人ページ
-        Route::get('{user}', 'Pages\PageController@show');
+        Route::get('{user}', 'Pages\PageController@show')->name('pages.page.show');
         // 作品一覧
-        Route::get('{user}/storages', 'Pages\StorageController@index');
+        Route::get('{user}/storages', 'Pages\StorageController@index')->name('pages.storage.index');
         // 作品ページ
-        Route::get('{user}/storages/{storage_id}', 'Pages\StorageController@show');
+        Route::get('{user}/storages/{storage_id}', 'Pages\StorageController@show')->name('pages.storage.show');
     });
 
     // 認証に通った場合
     Route::group(['middleware' => 'auth:api'], function () {
         // ログアウト
-        Route::post('logout', 'Auth\LoginController@logout');
+        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
         // TODO 必要ないと判断できたら削除
         Route::get('/user', function (Request $request) {
@@ -41,23 +41,29 @@ Route::group(['prefix' => 'v1'], function () {
         });
 
         // プロフィール一覧
-        Route::get('profiles', 'Pages\ProfileController@index');
+        Route::get('profiles', 'Pages\ProfileController@index')->name('pages.profile.index');
         // ストレージ一覧
-        Route::get('storages', 'Pages\StorageController@all');
+        Route::get('storages', 'Pages\StorageController@all')->name('pages.storage.all');
 
         Route::group(['prefix' => 'users'], function () {
             // ユーザー削除
-            Route::delete('', 'Users\UserController@destory');
+            Route::delete('', 'Users\UserController@destory')->name('user.destory');
             // 作品の一覧・追加・取得・編集・削除
-            Route::apiResource('storage', 'Users\StorageController');
+            Route::apiResource('storage', 'Users\StorageController')->names([
+                'index'   => 'user.storage.index',
+                'store'   => 'user.storage.store',
+                'show'    => 'user.storage.show',
+                'update'  => 'user.storage.update',
+                'destroy' => 'user.storage.destroy',
+            ]);
 
             // 自分のプロフィール取得・編集
-            Route::get('profile', 'Users\ProfileController@show');
-            Route::patch('profile', 'Users\ProfileController@update');
+            Route::get('profile', 'Users\ProfileController@show')->name('user.profile.show');
+            Route::patch('profile', 'Users\ProfileController@update')->name('user.profile.update');
 
             // 自分のポートフォリオ取得・編集
-            Route::get('page', 'Users\PageController@show');
-            Route::patch('page', 'Users\PageController@update');
+            Route::get('page', 'Users\PageController@show')->name('user.page.show');
+            Route::patch('page', 'Users\PageController@update')->name('user.page.update');
 
             // TODO 必要ないと判断できたら削除
             Route::patch('settings/profile', 'Settings\ProfileController@update');
@@ -67,8 +73,8 @@ Route::group(['prefix' => 'v1'], function () {
 
         // 未認証の場合
         Route::group(['middleware' => 'guest:api'], function () {
-            Route::post('login', 'Auth\LoginController@login');
-            Route::post('register', 'Auth\RegisterController@register');
+            Route::post('login', 'Auth\LoginController@login')->name('login');
+            Route::post('register', 'Auth\RegisterController@register')->name('register');
 
             Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
             Route::post('password/reset', 'Auth\ResetPasswordController@reset');
