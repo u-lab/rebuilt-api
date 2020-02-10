9c899348533dc33f404994f37d0f5a2bdfa8af55
H37kouya <h37kouya@gmail.com>
2020-02-10 19:00:53 +0900

    [add]: storng → userのリレーションシップを追加

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * App\Models\Storage
@@ -58,4 +60,12 @@ class Storage extends Model
      * @var array
      */
     protected $dates = ['deleted_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
