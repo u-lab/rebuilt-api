9db27d8b242eb1f53dae025ca6a8c0347a30535f
H37kouya <h37kouya@gmail.com>
2020-02-10 15:23:00 +0900

    [add]: storageのseederを追加

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(SnsAccountsTableSeeder::class);
+        $this->call(StoragesTableSeeder::class);
     }
 }
