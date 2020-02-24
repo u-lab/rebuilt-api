b2ae0736f5c77fafce8feccafd680d9b0dd6acfb
H37kouya <h37kouya@gmail.com>
2020-02-24 10:21:34 +0900

    breadcrumbsにエラーハンドリングの追加

--- app/Services/BreadcrumbsService.php
+++ app/Services/BreadcrumbsService.php
@@ -6,6 +6,8 @@ use Illuminate\Http\JsonResponse;
 use App\Http\Requests\BreadcrumbsFormRequest;
 use DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
 use App\Http\Resources\Breadcrumbs as BreadcrumbsResource;
+use DaveJamesMiller\Breadcrumbs\Exceptions\InvalidBreadcrumbException;
+use DaveJamesMiller\Breadcrumbs\Exceptions\UnnamedRouteException;
 
 class BreadcrumbsService
 {
@@ -13,10 +15,23 @@ class BreadcrumbsService
      * パンくずリストを生成する
      *
      * @param BreadcrumbsFormRequest $request
-     * @return mixed
+     * @return BreadcrumbsResource
+     * @throws \DaveJamesMiller\Breadcrumbs\Exceptions\UnnamedRouteException
+     * @throws \DaveJamesMiller\Breadcrumbs\Exceptions\InvalidBreadcrumbException
      */
-    public function render(BreadcrumbsFormRequest $request)
+    public function render(BreadcrumbsFormRequest $request): BreadcrumbsResource
     {
-        return new BreadcrumbsResource(Breadcrumbs::generate('blog'));
+        // TODO: $request を使って、パンくずリストを生成。
+        $breadcrumbs_name = 'blog';
+
+        try {
+            return new BreadcrumbsResource(Breadcrumbs::generate($breadcrumbs_name));
+        } catch (UnnamedRouteException $e) {
+            // TODO:Exception that is thrown if the user attempt to render breadcrumbs for the current route but the current route doesn't have a name.を書く
+            return abort(response()->json(['message' => $e->getMessage()], 500));
+        } catch (InvalidBreadcrumbException $e) {
+            // TODO: Exception that is thrown if the user attempts to generate breadcrumbs for a page that is not registered. を書く。
+            return abort(response()->json(['message' => $e->getMessage()], 500));
+        }
     }
 }
