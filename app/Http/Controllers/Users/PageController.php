187ae0e7ed94dc60ed5dbf2df03fde7223ddb917
H37kouya <h37kouya@gmail.com>
2020-02-24 12:50:59 +0900

    page/{user}にformrequestの追加

--- app/Http/Controllers/Users/PageController.php
+++ app/Http/Controllers/Users/PageController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\Users;
 use Illuminate\Http\Request;
 use App\Services\Users\PageService;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Users\ShowPageRequest;
 
 class PageController extends Controller
 {
     /**
      * @var \App\Services\Users\PageService
      */
-
     private $_service;
 
     public function __construct(PageService $service)
@@ -22,9 +22,10 @@ class PageController extends Controller
     /**
      * Display the specified resource.
      *
+     * @param ShowPageRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request)
+    public function show(ShowPageRequest $request)
     {
         return $this->_service->get_user_page($request);
     }
