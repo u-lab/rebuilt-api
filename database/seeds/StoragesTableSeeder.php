74877da083e7afaad731152faf7efa3c062753ba
H37kouya <h37kouya@gmail.com>
2020-02-27 13:47:14 +0900

    storageのeyecatch_imgae_urlをfakerからlocalhostに変更

--- database/seeds/StoragesTableSeeder.php
+++ database/seeds/StoragesTableSeeder.php
@@ -4,6 +4,7 @@ use Faker\Factory;
 use Carbon\Carbon;
 use App\Models\Storage;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
 
 class StoragesTableSeeder extends Seeder
 {
@@ -20,17 +21,17 @@ class StoragesTableSeeder extends Seeder
             $now = Carbon::now();
 
             Storage::create([
-                        'storage_id' => $i === 0 ?
-                            "1581315433ra05d0":
-                            $now->timestamp . 'ra'. $i % 10 .'5d' . $i % 10,
-                        'user_id' => 1,
-                        'title' => '作品' . ($i + 1) . 'かっこいいよ',
-                        'description' => $faker->sentence(3),
-                        'long_comment' => $faker->sentence(5),
-                        'storage_url' => $faker->url,
-                        'eyecatch_imgae_url' => $faker->url,
-                        'web_address' => $faker->url,
-                    ]);
+                'storage_id'         => $i === 0 ?
+                                            "1581315433ra05d0":
+                                            $now->timestamp . 'ra'. $i % 10 .'5d' . $i % 10,
+                'user_id'            => 1,
+                'title'              => '作品' . ($i + 1) . 'かっこいいよ',
+                'description'        => $faker->sentence(3),
+                'long_comment'       => $faker->sentence(5),
+                'storage_url'        => $faker->url,
+                'eyecatch_imgae_url' => FacadesStorage::disk('public')->url('storages/example/work1.jpg'),
+                'web_address'        => $faker->url,
+            ]);
         }
     }
 }
