4ee464ba1d2487fdc78eaf5e2c7bc91ab5386b44
H37kouya <h37kouya@gmail.com>
2020-03-01 21:55:57 +0900

    ImageRepositoryの作成

---
+++ app/Repositories/Image/ImageRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories\Image;
+
+use App\Models\Image;
+use App\Repositories\Image\ImageRepositoryInterface;
+
+class ImageRepository implements ImageRepositoryInterface
+{
+    /**
+     * @var \App\Models\Image
+     */
+    private $_image;
+
+    public function __construct(Image $image)
+    {
+        $this->_image = $image;
+    }
+}
