a21004ca1e72576b64509fddc09d1d6e04f58873
H37kouya <h37kouya@gmail.com>
2020-03-02 23:40:45 +0900

    requestを追加

--- app/Http/Requests/Users/StoreStorageRequest.php
+++ app/Http/Requests/Users/StoreStorageRequest.php
@@ -23,8 +23,40 @@ class StoreStorageRequest extends FormRequest
      */
     public function rules()
     {
+        /**
+         * obj => text/plain
+         * stl => application/octet-stream
+         * fbx => application/octet-stream
+         */
+
         return [
-            //
+            'title'          => ['required', 'string', 'max:50'],
+            'description'    => ['string', 'max:50'],
+            'long_comment'   => ['string', 'max:100000'],
+            'storage'        => [
+                'file',
+                'max:2048',
+                'mimetypes:'.$this->storage_minetypes()
+            ],
+            'eyecatch_image' => ['file', 'image', 'mimes:jpeg,png,jpg,svg', 'max:2048'],
+            'web_address'    => ['string', 'url' , 'max:255']
         ];
+    }
+
+    /**
+     * storageのminetypesのリスト
+     *
+     * @return string
+     */
+    protected function storage_minetypes(): string
+    {
+        $minetypes = [
+            'text/plain', /* obj */
+            'application/octet-stream' /* stl, fbx */
+            // 'application/x-tgif', /* obj, https://reposcope.com/mimetype/application/x-tgif */
+            // 'application/vnd.ms-pki.stl', /* stl */
+        ];
+
+        return implode(',', $minetypes);
     }
 }
