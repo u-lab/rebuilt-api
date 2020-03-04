f85515249917871353acd3c16221a0515c304dd4
H37kouya <h37kouya@gmail.com>
2020-03-04 14:24:57 +0900

    seederにImgaeを追加

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(SnsAccountsTableSeeder::class);
         $this->call(StoragesTableSeeder::class);
+        $this->call(ImageTableSeeder::class);
     }
 }
