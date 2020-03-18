2dccac521ede158659c7b939745d77aa0d111f22
H37kouya <h37kouya@gmail.com>
2020-03-18 22:44:26 +0900

    Breadcrumbの追加

--- app/Services/BreadcrumbsService.php
+++ app/Services/BreadcrumbsService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use Illuminate\Http\JsonResponse;
+use Exception;
 use App\Http\Requests\BreadcrumbsFormRequest;
 use DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
 use App\Http\Resources\Breadcrumbs as BreadcrumbsResource;
@@ -21,17 +21,73 @@ class BreadcrumbsService
      */
     public function render(BreadcrumbsFormRequest $request): BreadcrumbsResource
     {
-        // TODO: $request を使って、パンくずリストを生成。
-        $breadcrumbs_name = 'blog';
+        $path = $request->path;
 
         try {
-            return new BreadcrumbsResource(Breadcrumbs::generate($breadcrumbs_name));
+            $data = $this->get_breadcrumb_data($path);
+            if (is_null($data)) {
+                throw new Exception;
+            }
+
+            if (isset($data['data'])) {
+                return new BreadcrumbsResource(Breadcrumbs::generate($data['name'], $data['data']));
+            }
+
+            return new BreadcrumbsResource(Breadcrumbs::generate($data['name']));
         } catch (UnnamedRouteException $e) {
             // TODO:Exception that is thrown if the user attempt to render breadcrumbs for the current route but the current route doesn't have a name.を書く
             return abort(response()->json(['message' => $e->getMessage()], 500));
         } catch (InvalidBreadcrumbException $e) {
             // TODO: Exception that is thrown if the user attempts to generate breadcrumbs for a page that is not registered. を書く。
             return abort(response()->json(['message' => $e->getMessage()], 500));
+        } catch (Exception $e) {
+            return abort(response()->json(['message' => $e->getMessage()], 500));
         }
+    }
+
+    /**
+     * Breadcrumbs用のデータを作成する
+     *
+     * @param string $path
+     * @return array|null
+     */
+    protected function get_breadcrumb_data(string $path): ?array
+    {
+        $path = trim($path, '/');
+        $path_arr = explode("/", $path);
+        if ($path_arr === false) {
+            return null;
+        }
+        $path_arr_len = count($path_arr);
+
+        $ret_arr = [
+            'name' => null,
+            'data' => null
+        ];
+
+        // pages関連
+        if ($path_arr[0] === 'pages') {
+            $user = $path_arr[1];
+
+            if ($path_arr_len > 4) {
+                $storage_id = $path_arr[3];
+                $ret_arr['name'] = 'pages_storage_id';
+                $ret_arr['data'] = [
+                    'user'       => $user,
+                    'storage_id' => $storage_id
+                ];
+                return $ret_arr;
+            }
+
+            $ret_arr['name'] = 'pages_user';
+            $ret_arr['data'] = [
+                'user' => $user,
+            ];
+            return $ret_arr;
+        }
+
+        $ret_arr['name'] = $path_arr[0];
+
+        return $ret_arr;
     }
 }
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -7,8 +7,33 @@ Breadcrumbs::for('home', function ($trail) {
     $trail->push('Home', H::client_route('/'));
 });
 
-// Home > blog
-Breadcrumbs::for('blog', function ($trail) {
+// Home > [user]
+Breadcrumbs::for('pages_user', function ($trail, array $data) {
     $trail->parent('home');
-    $trail->push('Blog', H::client_route('/blog'));
+    $trail->push($data['user'], H::client_route('/pages' . '/' . $data['user']));
+});
+
+// Home > [user] > [storage_id]
+Breadcrumbs::for('pages_storage_id', function ($trail, array $data) {
+    $trail->parent('pages_user');
+    $path = '/pages' . '/' . $data['user'] . '/storages' . '/' . $data['storage_id'];
+    $trail->push($data['storage_id'], H::client_route($path));
+});
+
+// Home > login
+Breadcrumbs::for('login', function ($trail) {
+    $trail->parent('home');
+    $trail->push('Login', H::client_route('/login'));
+});
+
+// Home > register
+Breadcrumbs::for('register', function ($trail) {
+    $trail->parent('home');
+    $trail->push('Register', H::client_route('/register'));
+});
+
+// Home > sitemap
+Breadcrumbs::for('sitemap', function ($trail) {
+    $trail->parent('home');
+    $trail->push('Sitemap', H::client_route('/sitemap'));
 });
