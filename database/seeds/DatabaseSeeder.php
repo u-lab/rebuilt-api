72a9bfa45e0909c46e9e728b9c6f527ab30061d2
H37kouya <h37kouya@gmail.com>
2020-03-21 16:56:49 +0900

    tableのseederの追加と修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(ReleaseTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(SnsAccountsTableSeeder::class);
