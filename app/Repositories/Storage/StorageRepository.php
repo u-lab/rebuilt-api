513b4c5eee5ce1e8671b83bfcc3c6bd11eda7b22
H37kouya <h37kouya@gmail.com>
2020-04-05 17:35:04 +0900

    storageのeager loadの追加

--- app/Repositories/Storage/StorageRepository.php
+++ app/Repositories/Storage/StorageRepository.php
@@ -59,7 +59,35 @@ class StorageRepository implements StorageRepositoryInterface
      */
     public function get_all_storages(int $per_page = 15): LengthAwarePaginator
     {
-        return $this->_storage->with(['user', 'eyecatch_image'])->paginate($per_page);
+        $storage = $this->_storage
+            ->with([
+                'user',
+                'user.user_role',
+                'user.user_role.role',
+                'user.user_info',
+                'user.user_profile',
+                'user.user_profile.icon_image',
+                'user.user_profile.background_image',
+                'user.user_profile.user_career',
+                'user.user_portfolio',
+                'user.user_portfolio.storage',
+                'user.user_sns_accounts',
+                'user.user_sns_accounts.sns_account',
+                'user.user_release',
+                'user.user_release.release',
+                'eyecatch_image',
+                'release',
+                'storage_file',
+                'storage_sub_image',
+                'storage_sub_image.image'
+            ])->whereHas('release', function ($query) {
+                $query->where('release_level', '=', 100);
+            })->whereHas('user.user_role.role', function ($query) {
+                $query->where('role_level', '>=', 10);
+            })->whereHas('user.user_release.release', function ($query) {
+                $query->where('release_level', '=', 100);
+            })->paginate($per_page);
+        return $storage;
     }
 
     /**
@@ -73,8 +101,21 @@ class StorageRepository implements StorageRepositoryInterface
     public function get_storage(int $user_id, string $storage_id)
     {
         $storage = $this->_storage->where('user_id', '=', $user_id)
+            ->with([
+                'user',
+                'user.user_sns_accounts',
+                'eyecatch_image',
+                'storage_file',
+                'storage_sub_image'
+            ])
             ->where('storage_id', '=', $storage_id)
-            ->firstOrFail();
+            ->whereHas('release', function ($query) {
+                $query->where('release_level', '=', 100);
+            })->whereHas('user.user_role.role', function ($query) {
+                $query->where('role_level', '>=', 10);
+            })->whereHas('user.user_release.release', function ($query) {
+                $query->where('release_level', '=', 100);
+            })->firstOrFail();
 
         return $storage;
     }
@@ -89,9 +130,21 @@ class StorageRepository implements StorageRepositoryInterface
     public function get_storage_no_user_id(string $storage_id)
     {
         $storage = $this->_storage
+            ->with([
+            'user',
+            'release',
+            'eyecatch_image',
+            'storage_file',
+            'storage_sub_image'
+            ])
             ->where('storage_id', '=', $storage_id)
-            ->with('user')
-            ->firstOrFail();
+            ->whereHas('release', function ($query) {
+                $query->where('release_level', '=', 100);
+            })->whereHas('user.user_role.role', function ($query) {
+                $query->where('role_level', '>=', 10);
+            })->whereHas('user.user_release.release', function ($query) {
+                $query->where('release_level', '=', 100);
+            })->firstOrFail();
 
         return $storage;
     }
@@ -106,11 +159,15 @@ class StorageRepository implements StorageRepositoryInterface
      */
     public function get_user_all_storages(int $user_id, int $per_page = 15): LengthAwarePaginator
     {
-        return $this->_storage
-                ->with('eyecatch_image')
+        $storage = $this->_storage
+                ->with(['release', 'eyecatch_image', 'storage_file', 'storage_sub_image'])
                 ->whereUserId($user_id)
+                ->whereHas('release', function ($query) { // banされているやつは除外
+                    $query->where('release_level', '>', 5);
+                })
                 ->orderBy('updated_at', 'desc')
                 ->paginate($per_page);
+        return $storage;
     }
 
     /**
