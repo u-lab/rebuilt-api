c88db60a19907499bc554df3155d6bf65699bcf1
H37kouya <h37kouya@gmail.com>
2020-04-05 20:09:45 +0900

    eager loadを使用

--- app/Repositories/User/UserProfileRepository.php
+++ app/Repositories/User/UserProfileRepository.php
@@ -27,7 +27,9 @@ class UserProfileRepository implements UserProfileRepositoryInterface
      */
     public function get_user_profile_by_id(string $user_id)
     {
-        return $this->_userProfile->findOrFail($user_id);
+        return $this->_userProfile
+            ->with(['icon_image', 'background_image', 'user_career'])
+            ->findOrFail($user_id);
     }
 
     /**
