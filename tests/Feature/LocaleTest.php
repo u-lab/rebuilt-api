26f80d922908c7b86715dbc4dd7d0092bb4034be
H37kouya <h37kouya@gmail.com>
2020-04-16 22:22:38 +0900

    testの修正

--- tests/Feature/LocaleTest.php
+++ tests/Feature/LocaleTest.php
@@ -9,17 +9,17 @@ class LocaleTest extends TestCase
     /** @test */
     public function set_locale_from_header()
     {
-        $this->withHeaders(['Accept-Language' => 'zh-CN'])
-            ->postJson('/api/login');
+        $this->withHeaders(['Accept-Language' => 'ja'])
+            ->postJson(route('login'));
 
-        $this->assertEquals('zh-CN', $this->app->getLocale());
+        $this->assertEquals('ja', $this->app->getLocale());
     }
 
     /** @test */
     public function set_locale_from_header_short()
     {
         $this->withHeaders(['Accept-Language' => 'en-US'])
-            ->postJson('/api/login');
+            ->postJson(route('login'));
 
         $this->assertEquals('en', $this->app->getLocale());
     }
